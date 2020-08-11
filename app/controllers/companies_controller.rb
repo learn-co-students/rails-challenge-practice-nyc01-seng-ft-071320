1e6d9997740895bc955f8a24b2ec58b4bc9d17fe
pairofAces <karanchauhan16@gmail.com>
2020-08-10 22:28:35 -0400

    Done.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,40 @@
 class CompaniesController < ApplicationController
+    def index
+        @companies = Company.all
+    end
+
+    def show
+        @company = Company.find(params[:id])
+    end
+
+    def new
+        @company = Company.new
+    end
+
+    def edit
+        @company = Company.find(params[:id])
+    end
+
+    def create
+        @company = Company.create(strong_params)
+        redirect_to company_path(@company)
+    end
+
+    def update
+        @company = Company.find(params[:id])
+        @company.update(strong_params)
+        redirect_to company_path
+    end
+
+    def destroy
+        @company = Company.find(params[:id])
+        @company.destroy
+        redirect_to company_path
+    end
+
+    private
+
+    def strong_params
+        params.require(:company).permit(:name)
+    end
 end
